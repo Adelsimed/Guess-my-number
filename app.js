e7d22fa2a8abcdb4b0c2ccdaddbf999223337d64
adelsimed <adelsimohamed@gmail.com>
2023-04-26 16:44:09 +0100

    Handling the click events of check btn.

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+// document.querySelector(".message").textContent = "🥳 correct Number ";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// document.querySelector(".guess").value = 23;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "🛑  No number!";
+  }
+});
